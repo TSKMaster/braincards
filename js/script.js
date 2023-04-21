0777345cc90d7edc2905dfca6ce013636b4b299d
TSKMaster <tskmaster.pvl@gmail.com>
2023-04-21 15:36:00 +0600

    Day1

--- js/script.js
+++ js/script.js
@@ -1,18 +1,38 @@
+import { createCategory } from "./components/createCategory.js";
 import { createHeader } from "./components/createHeader.js";
+import { createElement } from "./helper/createElement.js";
+import { fetchCategories } from "./service/api.service.js";
 
-const initApp = () => {
+const initApp = async () => {
   const headerParent = document.querySelector('.header');
   const app = document.querySelector('#app');
   
   const headerObj = createHeader(headerParent);
-  console.log('headerObj: ', headerObj);
+  const categoryObj = createCategory(app);
+  
+  
+  
+  
 
-  const returnIndex = (e) => {
-    e.preventDefault();
-    headerObj.updateHeaderTitle('Категории')
+  const renderIndex = async(e) => {
+    e?.preventDefault();
+    const categories = await fetchCategories();
+  if (categories.error){
+    const errorText = createElement('p',{
+      className: 'server-error',
+      textContent: 'Ошибка сервера, попробуйте зайти позже',
+    });
+    app.append(errorText);
+    return;
+  }
+
+  categoryObj.mount(categories);
   };
-  headerObj.headerLogoLink.addEventListener('click', returnIndex)
+  renderIndex();
+
+  headerObj.headerLogoLink.addEventListener('click', renderIndex)
   headerObj.headerBtn.addEventListener('click', () => {
+    categoryObj.unmount();
     headerObj.updateHeaderTitle('Новая категория')
   })
 
